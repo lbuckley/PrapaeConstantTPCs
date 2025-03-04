b7af6841697464064b2b0d83a6bbdabf0a67aaa7
Lauren Buckley <l.b.buckley@gmail.com>
2025-03-04 13:57:51 -0800

    minor updates and diagnose time dependence

--- 24hrTPCConstantAnalysis.R
+++ 24hrTPCConstantAnalysis.R
@@ -11,6 +11,9 @@ library(reshape2)
 library(viridis)
 library(tidyverse)
 library(lubridate)
+library(nlme)
+library(lme4)
+library(sjPlot)
 
 #toggle between desktop (y) and laptop (n)
 desktop<- "y"
@@ -63,7 +66,7 @@ tpc.c$fw<- as.numeric(tpc.c$fw)
 
 #Calculate mass gained in each time treatment
 tpc.c$mgain= tpc.c$fw - tpc.c$M0
-#FIX: check big negative and positive values
+#FIX: CHECK big negative and positive values
 
 # Paste date in t.in and t.out column to make duration calculation easier
 tpc.c$t.in <- paste(tpc.c$Date, tpc.c$t.in, sep = " ")
@@ -87,6 +90,7 @@ tpc.c <- tpc.c %>%
 
 #FIX
 #CHECK time NAs
+#CHECK current times that deviate from 6 and 24 hrs
 tpc.c[which(tpc.c$duration>30),]
 
 # Make sure that new data follows naming of old data sets
@@ -107,24 +111,53 @@ tpc.ps$mom <- as.character(tpc.ps$mom)
 # Combine past and current datasets while preserving time.per
 tpc <- rbind(tpc.cs, tpc.ps) 
 
+#align active
+tpc$active[which(tpc$active %in% c("yes","y?"))]<- "y"
+tpc$active[which(tpc$active %in% c("no","n?"))]<- "n"
+
 # Save data frame to new Csv
-#write.csv(tpc, "PastPresentFilteredConstantTpc2024.csv")
+if(desktop=="y") setwd("/Users/laurenbuckley/Google Drive/My Drive/Buckley/Work/WARP/projects/TPCconstant/out/")
+if(desktop=="n") setwd("/Users/lbuckley/Library/CloudStorage/GoogleDrive-lbuckley@uw.edu/My Drive/Buckley/Work/WARP/projects/TPCconstant/out/")
+
+write.csv(tpc, "PastPresentFilteredConstantTpc2024.csv")
 
 #==========================
 #analysis
 # Filter caterpillars to include the ones who were active
-tpc <- tpc %>% filter(active %in% c("y","yes","y?"))
+tpc <- tpc %>% filter(active %in% c("y"))
+
+#drop initial zero time estimates
+tpc<- tpc[which(tpc$time>0),]
 
 # calculate relative growth rate using logarithmic scale for 1999 past data set
 #Kingsolver and Gomulkiewicz 2003
 #“We have quantified short-term (6 hr) growth rates of P. rapae caterpillars at different temperatures in terms of the relative growth rate (RGR), defined as RGR =[ln(mf / mi )]/(tf - ti ), where mi is initial mass at time ti , and mf is final mass at time tf . Figure 1 shows short-term RGR as a function of temperature for 4th and 5th instar P. rapae from Seattle WA (Kingsolver, 2000).”
 #paper plot matches log10 not ln
-tpc$rgrlog= log10(tpc$fw/tpc$Mo)/tpc$time 
-#OR with conversion to mg: tpc$rgrlog= (log10(tpc$fw*0.001)-log10(tpc$Mo*0.001))/tpc$time 
+#tpc$rgrlog= log10(tpc$fw/tpc$Mo)/tpc$time 
+#OR with conversion to mg: 
+tpc$rgrlog= (log10(tpc$fw*0.001)-log10(tpc$Mo*0.001))/tpc$time 
 
 # calculate relative growth rate using arithmetic scale 
 #tpc$rgrarith = (tpc$fw/tpc$Mo) / tpc$time 
 
+#--------
+#check time dependence of feeding
+tpc1<- tpc[which(tpc$time.per=="past"),]
+
+#restrict to potential time limits
+mod= lm(rgrlog ~ temp*time, data= tpc1[which(tpc1$time>5 & tpc1$time<10 ),]) 
+mod= lm(rgrlog ~ temp*time, data= tpc1[which(tpc1$time>21 & tpc1$time<26),]) 
+anova(mod)
+
+plot_model(mod, type = "pred", terms = c("time", "temp"), show.data=TRUE)
+
+mod.lmer <- lme(rgrlog ~ temp*time, random=~1|mom, data = na.omit(tpc1[which(tpc1$time>5 & tpc1$time<10),]))
+mod.lmer <- lme(rgrlog ~ temp*time, random=~1|mom, data = na.omit(tpc1[which(tpc1$time>21 & tpc1$time<26),]))
+anova(mod.lmer)
+
+plot_model(mod.lmer, type = "pred", terms = c("time", "temp"), show.data=TRUE)
+
+#-----
 #set up 6 and 24 hr studies
 tpc$time.class<- NA
 #expand windows
@@ -134,7 +167,7 @@ tpc$time.class[which(tpc$time>21 & tpc$time<26)]<-24
 #---------------------
 #PLOT
 
-#plot feeding rate over time
+#plot feeding rate over time to assess time adjustments
 tpc.plot <- tpc[which(tpc$time>0),]
 tpc.plot$mass.r<- log10(tpc.plot$fw/tpc.plot$Mo)
 
@@ -199,7 +232,7 @@ rgrtime.plot <- ggplot(tpc.agg, aes( x = temp, y = mean, color = time.per, lty=f
   facet_grid(instar ~ .) 
 
 #initial weights
-plot.mass<- ggplot(tpc, aes(x=Mo,color=time.per, group=time.per)) + 
+plot.mass<- ggplot(tpc, aes(x=log10(Mo),color=time.per, group=time.per)) + 
   geom_density(aes(fill=time.per), alpha=0.5)+
   ylab("Density") +xlab("mass (mg)")+
   facet_wrap(.~instar)+
